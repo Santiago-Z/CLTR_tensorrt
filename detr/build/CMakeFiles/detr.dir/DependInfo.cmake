2b46f4496d49de2ee9a23dee63b8c6cacb220c17
root <root@LAPTOP-EM45A76I.localdomain>
2025-09-04 11:34:38 +0800

    add cpp code form tensorrtx

---
+++ detr/build/CMakeFiles/detr.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/01_Work/opt/AI_proj/CLTR_tensorrt/detr/detr.cpp" "/mnt/d/01_Work/opt/AI_proj/CLTR_tensorrt/detr/build/CMakeFiles/detr.dir/detr.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/cuda/include"
+  "/data/app/TensorRT-8.4.3.1/include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
